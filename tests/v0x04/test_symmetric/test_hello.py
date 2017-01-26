2cc1a91cf2963a4f71b5e23b3a2301221260fb88
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-01-26 10:07:43 -0200

    Adding a new test for the Hello message on v0x04

--- tests/v0x04/test_symmetric/test_hello.py
+++ tests/v0x04/test_symmetric/test_hello.py
@@ -13,3 +13,13 @@ class TestHello(TestStruct):
         super().set_raw_dump_file('v0x04', 'ofpt_hello')
         super().set_raw_dump_object(Hello, xid=1)
         super().set_minimum_size(8)
+
+    def test_hello_attributes(self):
+        obj = self.get_raw_object()
+        print(obj, obj.header, obj.header.version, obj.header.message_type)
+        self.assertEqual(obj.header.version, 4)
+        self.assertEqual(obj.header.message_type, 0)
+        self.assertEqual(obj.header.xid, 1)
+        obj.update_header_length()
+        self.assertEqual(obj.header.length, 8)
+

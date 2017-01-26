c2d47cdcc878957585d324d7e699ad2287a2e56d
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-01-26 08:22:33 -0200

    Creating a Enum of OF Versions

--- pyof/foundation/constants.py
+++ pyof/foundation/constants.py
@@ -1,5 +1,7 @@
 """General constants from python-openflow library."""
 
+from enum import Enum
+
 # Max values of each basic type
 UBINT8_MAX_VALUE = 255
 UBINT16_MAX_VALUE = 65535
@@ -11,3 +13,12 @@ OFP_MAX_PORT_NAME_LEN = 16
 OFP_MAX_TABLE_NAME_LEN = 32
 SERIAL_NUM_LEN = 32
 DESC_STR_LEN = 256
+
+
+class PyofVersion(Enum):
+    """Enumeration of openflow versions implemented (available) on the lib."""
+
+    v0x01 = 0x01
+    # v0x02 = 0x02
+    # v0x03 = 0x03
+    v0x04 = 0x04
